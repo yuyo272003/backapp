822d13734aebf10472d070cec67e4b2b658bbf65
julio <zS2201737@estudiantes.uv.mx>
2025-03-24 21:46:41 -0600

    funciona el register

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,18 +14,18 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->nullable();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password')->nullable();
+//            $table->string('email')->nullable();
+//            $table->timestamp('email_verified_at')->nullable();
+//            $table->string('password')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
 
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
+//        Schema::create('password_reset_tokens', function (Blueprint $table) {
+//            $table->string('email')->primary();
+//            $table->string('token');
+//            $table->timestamp('created_at')->nullable();
+//        });
 
         Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
